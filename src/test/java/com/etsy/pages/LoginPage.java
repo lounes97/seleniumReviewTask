72354184e55fb2964d5ec7021118164a710b418d
Adilya Danil <84361675+adilyadanil@users.noreply.github.com>
2023-06-07 17:19:45 -0700

    task3

--- src/test/java/com/etsy/pages/LoginPage.java
+++ src/test/java/com/etsy/pages/LoginPage.java
@@ -5,18 +5,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class LoginPage {
-    public LoginPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
     @FindBy(name = "username")
     public WebElement userNameBox;
 
     @FindBy(name = "password")
     public WebElement passwordBox;
 
-    @FindBy (xpath = "//button[.='Login']")
+    @FindBy(xpath = "//button[.='Login']")
     public WebElement loginBtn;
+    @FindBy(xpath = "//button[.='Order']")
+    public WebElement orderBtn;
+    @FindBy(xpath = "//select[@name='product']")
+    public WebElement productDropDown;
+    @FindBy(xpath = "//input[@value='Visa']")
+    public WebElement visaCard;
+
+    @FindBy(xpath = "//input[@value='MasterCard']")
+    public WebElement masterCard;
+
+    @FindBy(xpath ="//input[@value='American Express']" )
+    public WebElement americanExpressCard;
+
 
 
 }
--- src/test/java/com/etsy/stepDef/login_StepDef.java
+++ src/test/java/com/etsy/stepDef/login_StepDef.java
@@ -1,16 +1,23 @@
 package com.etsy.stepDef;
 
 import com.etsy.pages.LoginPage;
+import com.etsy.utilities.BrowserUtil;
 import com.etsy.utilities.ConfigurationReader;
 import com.etsy.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class login_StepDef {
 
 
-    LoginPage loginPage =new LoginPage();
+    LoginPage loginPage = new LoginPage();
 
     @Given("user open the website on home page")
     public void user_open_the_website_on_home_page() throws InterruptedException {
@@ -19,6 +26,7 @@ public class login_StepDef {
         Thread.sleep(2000);
 
     }
+
     @Then("user enter the username {string}")
     public void user_enter_the_username(String userName) throws InterruptedException {
         loginPage.userNameBox.sendKeys(userName);
@@ -26,6 +34,7 @@ public class login_StepDef {
 
 
     }
+
     @Then("user enter the password {string}")
     public void user_enter_the_password(String password) throws InterruptedException {
         loginPage.passwordBox.sendKeys(password);
@@ -33,17 +42,53 @@ public class login_StepDef {
 
 
     }
+
     @Then("user clicks login button")
     public void user_clicks_login_button() throws InterruptedException {
         loginPage.loginBtn.click();
         Thread.sleep(2000);
 
     }
+
     @Then("user verify if the URL ends with {string}")
     public void user_verify_if_the_url_ends_with(String keyWord) {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(keyWord));
 
+    }
 
+    //Scenario3
+    @Given("user click on order option")
+    public void user_click_on_order_option() throws InterruptedException {
+        loginPage.orderBtn.click();
+        Thread.sleep(2000);
+    }
+
+    @Then("user see all the three options")
+    public void user_see_all_the_three(List<String> expectedOptions) {
+        Select select = new Select(loginPage.productDropDown);
+        List<WebElement> options = select.getOptions();
+        List<String> elementsText = BrowserUtil.getElementsText(options);
+        Assert.assertEquals(expectedOptions, elementsText);
+    }
+
+
+    @Then("user clicks on ORDER")
+    public void userClicksOnORDER() {
+
+        loginPage.orderBtn.click();
 
     }
+
+    @And("user can see the radio button to choose the card type")
+    public void userCanSeeTheRadioButtonToChooseTheCardType(List<String> expectedCardsType) {
+        List<String> actualCardsType = new ArrayList<>();
+        actualCardsType.add(loginPage.visaCard.getAttribute("value"));
+        actualCardsType.add(loginPage.masterCard.getAttribute("value"));
+        actualCardsType.add(loginPage.americanExpressCard.getAttribute("value"));
+
+
+        Assert.assertEquals(expectedCardsType, actualCardsType);
+
+    }
+
 }
