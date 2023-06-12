9ee2088737b079da434c9009041a90365f05f580
lounes_hd <zizoulounes@gmail.com>
2023-06-12 15:03:32 -0500

    forget password

--- src/test/java/com/etsy/pages/ForgetPasswordPage.java
+++ src/test/java/com/etsy/pages/ForgetPasswordPage.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.support.PageFactory;
 public class ForgetPasswordPage {
 
     public ForgetPasswordPage() {
+
         PageFactory.initElements(Driver.getDriver(), this);
+
     }
 
     @FindBy(css = "a[href='/']")
