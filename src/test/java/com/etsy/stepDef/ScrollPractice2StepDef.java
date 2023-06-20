c793d06a50b84097660849195eadcf91e153c856
Adilya Danil <84361675+adilyadanil@users.noreply.github.com>
2023-06-20 11:56:12 -0700

    ScrollPractice2 task is done

--- src/test/java/com/etsy/stepDef/ScrollPractice2StepDef.java
+++ src/test/java/com/etsy/stepDef/ScrollPractice2StepDef.java
@@ -10,13 +10,15 @@ public class ScrollPractice2StepDef {
     ScrollPractice2Page scrollPractice2Page=new ScrollPractice2Page();
 
     @Then("user scroll down to {string} link")
-    public void user_scroll_down_to_link(String string) {
+    public void user_scroll_down_to_link(String string) throws InterruptedException {
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);",scrollPractice2Page.cydeoLink);
+        Thread.sleep(3000);
     }
     @Then("user scroll up to {string} link")
-    public void user_scroll_up_to_link(String string) {
+    public void user_scroll_up_to_link(String string) throws InterruptedException {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);",scrollPractice2Page.homeLink);
+        Thread.sleep(3000);
 
-        scrollPractice2Page.homeLink.click();
     }
 
 }
